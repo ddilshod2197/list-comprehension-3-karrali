60916136992298f1d02ac47e93033f7bbc196b95
Dilshod <ddilshod2197@gmail.com>
2026-05-02 16:58:26 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,13 @@
+def kvadratlash(ro'yxat):
+    return list(map(lambda x: x**2, ro'yxat))
+
+ro'yxat = [1, 2, 3, 4, 5]
+print(kvadratlash(ro'yxat))
+```
+
+```python
+def kvadratlash(ro'yxat):
+    return list(map(lambda x: x**2, ro'yxat))
+
+ro'yxat = [1, 2, 3, 4, 5]
+print(kvadratlash(ro'yxat))  # [1, 4, 9, 16, 25]
